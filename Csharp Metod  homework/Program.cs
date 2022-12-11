28e205e33b6b8280f278f76d1eea1b00728ce2e1
Faridrab <faridrab@code.edu.az>
2022-12-11 09:55:39 -0800

    Metod homework

---
+++ Csharp Metod  homework/Program.cs
@@ -0,0 +1,240 @@
+﻿
+
+
+#region task1
+
+
+//IsDividedOrNot(21);
+
+
+//static void IsDividedOrNot(int n)
+//{
+
+
+
+//    if (n % 3 == 0 && n % 7 == 0)
+//    {
+//        Console.WriteLine("bolunur");
+
+//    }
+//    else
+//    {
+//        Console.WriteLine("bolunmur");
+//    }
+
+
+//}
+#endregion
+
+
+
+
+#region task2
+
+
+//SumOfNumbers(10, 20);
+
+//static void SumOfNumbers(int n, int m)
+//{
+
+//    if (n % 2 == 0 && m % 2 == 0)
+//    {
+//        Console.WriteLine(n + m);
+//    }
+//    else
+//    {
+//        Console.WriteLine("duzgun eded daxil edin");
+//    }
+
+
+//}
+
+#endregion
+
+#region task3
+
+//SumOfOddNumbers(1, 10);
+
+//static void SumOfOddNumbers(int n, int m)
+//{
+//    int sum = 0;
+
+
+
+//    for (int i = n; i < m; i++)
+//    {
+//        if (i % 2 == 1)
+//        {
+//            sum += i;
+//        }
+//    }
+//    Console.WriteLine(sum);
+
+//}
+
+#endregion
+
+#region task4
+
+
+
+
+//int[] number = { 1, 5, 23, 7, 5, 8, 9, 4, };
+
+//SumOfOddNumbers(number);
+//static void SumOfOddNumbers(int[] number)
+
+//{
+//    int result = 0;
+//    for (int i = 0; i < number.Length; i++)
+//    {
+//        if (number[i] % 2 == 1)
+//        {
+//            result = result + number[i];
+//        }
+//    }
+//    Console.WriteLine(result);
+
+//}
+
+#endregion
+
+
+#region task5
+
+
+//int[] number = { 12, 5, 8, 7, 5, 8, 3, 4 };
+
+//Number(number);
+//static void Number(int[] number)
+//{
+//    int result = 0;
+//    for (int i = 0; i < number.Length; i++)
+//    {
+//        if (number[i] % 2 == 0)
+//        {
+//            result++;
+//        }
+//    }
+//    Console.WriteLine(result);
+
+//}
+
+#endregion
+
+#region task6
+
+
+//Number(1, 10);
+//static void Number(int n, int m)
+//{
+//    int count = 0;
+
+//    for (int i = n; i < m; i++)
+//    {
+//        if (i % 2 == 1)
+//        {
+//            count++;
+//        }
+//    }
+//    Console.WriteLine(count);
+//}
+
+#endregion
+
+#region task7
+
+//SumOfNumbers(1, 10);
+//static void SumOfNumbers(int n, int m)
+//{
+//    int sum = 0;
+
+
+//    for (int i = n; i < m; i++)
+//    {
+//        if (i % 2 == 0)
+//        {
+//            sum += i;
+//        }
+//    }
+//    Console.WriteLine(sum);
+//}
+
+#endregion
+
+
+#region task8
+
+
+//Number(1, 10);
+//static void Number(int n, int m)
+//{
+//	int count = 0;
+
+//	for (int i = n; i < m; i++)
+//	{
+//		if (i % 2 == 0)
+//		{
+//			count++;
+//		}
+//	}
+//	Console.WriteLine(count);
+//}
+#endregion
+
+
+#region task9
+
+//SimpleOrİnk(21);
+//static void SimpleOrİnk(int n)
+
+//{
+//    int count = 0;
+//    if (n < 0)
+//    {
+//        Console.WriteLine("duzgun reqem yazzzz");
+//    }
+//    else if (n == 1)
+//    {
+//        Console.WriteLine("ne sadedir nedeki murekkeb");
+//    }
+//    else
+//    {
+//        for (int i = 1; i <= n; i++)
+//        {
+//            if (n % i == 0)
+//            {
+//                count++;
+//            }
+//        }
+//        if (count > 2)
+//        {
+//            Console.WriteLine("murekkebdir");
+//        }
+//        else
+//        {
+//            Console.WriteLine("sadedir");
+//        }
+
+//    }
+
+
+//}
+#endregion
+
+#region task10
+
+
+//Faktorial(5);
+//static void Faktorial(int n)
+//{
+//	int multiple = 1;
+//	for (int i = 1; i <= n; i++)
+//	{
+//		multiple *= i;
+//	}
+//	Console.WriteLine(multiple);
+//}
+
+#endregion
+
